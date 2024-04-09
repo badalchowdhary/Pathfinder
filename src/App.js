956246efbd7719e9c4e0d64afb8a40592a65f27b
Badal <badal.chowdhary@iitgn.ac.in>
2024-04-09 13:31:27 +0530

    title and favicon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import PathfindingVisualizer from './pages/pathFindingVisualizer';
 import Navbar from './pages/Navbar.jsx';
+import {Helmet} from "react-helmet";
 
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pathfinder</title>
+        <link rel="canonical" href="https://pathfinder-badal-chowdharys-projects.vercel.app/" />
+      </Helmet>
       <Navbar/>
       <PathfindingVisualizer/>
     </>
